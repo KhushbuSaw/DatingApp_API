e89bc2bc90f42dc087e075138c84fcbd39abbc4e
Khushbu Saw <khushbusaw76@gmail.com>
2024-07-02 22:24:51 +0530

    Add project files.

---
+++ DatingApp_Infrastructure/DbContext/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatingApp_Shared.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp_Infrastructure
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options):base(options)
+        {
+                
+        }
+        public DbSet<AppUserEntity> AppUser { get; set; }
+    }
+}
